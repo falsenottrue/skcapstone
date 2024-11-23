0a455eddcb6ae7397f22a48901ebf82d09e2d8ba
falsenottrue <paulwilliamtrinidad@gmail.com>
2024-11-23 22:21:37 +0800

    /

--- register.php
+++ register.php
@@ -2,10 +2,7 @@
 
 include 'connection.php';
 session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: index.php");
-    exit();
-}
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
